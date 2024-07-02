444b5be320e2d20ad34afd1621bebfab45443284
sampsone <esampson@suse.com>
2024-07-02 11:09:40 -0500

    fix change uptime_data to uptimeData

--- java/code/src/com/redhat/rhn/domain/scc/SCCCachingFactory.java
+++ java/code/src/com/redhat/rhn/domain/scc/SCCCachingFactory.java
@@ -601,13 +601,13 @@ public class SCCCachingFactory extends HibernateFactory {
     /**
      * Return list of data for last seen SCC update call
      * @param cred SCC Org Crednetials
-     * @return a list of maps with the keys scc_login, scc_passwd, checkin
+     * @return a list of maps with the keys scc_login, scc_passwd, checkin, uptimeData
      */
     @SuppressWarnings("unchecked")
     public static List<Map<String, Object>> listUpdateLastSeenCandidates(SCCCredentials cred) {
         List<Map<String, Object>> result = new ArrayList<>();
         List<Object[]> rows = getSession().createQuery(
-                "SELECT reg.sccLogin, reg.sccPasswd, si.checkin, si.uptime_data " +
+                "SELECT reg.sccLogin, reg.sccPasswd, si.checkin, si.uptimeData " +
                 "FROM com.redhat.rhn.domain.scc.SCCRegCacheItem AS reg " +
                 "JOIN reg.server AS s " +
                 "JOIN s.serverInfo AS si " +
